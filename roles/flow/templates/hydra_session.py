0a5bb5c4409746fc258a78e1dc8e1acd41de3dcf
Edwin Balani <eb677@srcf.net>
2023-04-10 22:45:35 +0100

    Provide a HydraClient to wrap the Hydra admin API

--- roles/flow/templates/hydra_session.py
+++ roles/flow/templates/hydra_session.py
@@ -12,6 +12,9 @@ from urllib3.connection import HTTPConnection
 from urllib3.connectionpool import HTTPConnectionPool
 from requests.adapters import HTTPAdapter
 
+from typing import Optional
+
+
 class Connection(HTTPConnection):
     def __init__(self):
         super().__init__("localhost")
@@ -20,6 +23,7 @@ class Connection(HTTPConnection):
         self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         self.sock.connect("{{ hydra_admin_api }}")
 
+
 class ConnectionPool(HTTPConnectionPool):
     def __init__(self):
         super().__init__("localhost")
@@ -27,9 +31,11 @@ class ConnectionPool(HTTPConnectionPool):
     def _new_conn(self):
         return Connection()
 
+
 class Adapter(HTTPAdapter):
     def get_connection(self, url, proxies=None):
         return ConnectionPool()
+
 
 class HydraSession(requests.Session):
     def __init__(self):
@@ -40,6 +46,33 @@ class HydraSession(requests.Session):
         # Strip leading '/' on provided `url` values to stop urljoin() from
         # removing the required '/admin/' subpath
         url = urljoin("http://hydra/admin/", url.lstrip('/'))
-        return super(HydraSession, self).request(method, url, **kwargs)
+        return super().request(method, url, **kwargs)
+
+
+class HydraClient:
+    def __init__(self):
+        self._session = HydraSession()
+
+    def _get(self, *args, **kwargs):
+        return self._session.get(*args, **kwargs)
+
+    def _post(self, *args, **kwargs):
+        return self._session.post(*args, **kwargs)
+
+    def get_client_by_id(self, client_id: str):
+        resp = self._get(f"/clients/{client_id}")
+        if resp.status_code == 200:
+            return resp.json(), resp
+        return None, resp
+
+    def introspect_token(self, token: str, scopes: Optional[str] = None):
+        request_data = {"token": token}
+        if scopes is not None:
+            request_data["scopes"] = scopes
+        resp = self._post("/oauth2/introspect", data=request_data)
+        if resp.status_code == 200:
+            return resp.json(), resp
+        return None, resp
+
 
 session = HydraSession()
